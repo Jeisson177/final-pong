48f74c13838ea452706510bac6fe45923db2f1a7
Jeisson177 <79467624+Jeisson177@users.noreply.github.com>
2021-11-21 14:35:42 -0500

    Update README.vhd

--- README.vhd
+++ README.vhd
@@ -1,198 +1,30 @@
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.NUMERIC_STD.ALL;
-
-ENTITY PONG IS
-	PORT (	clk		:	IN		STD_LOGIC;
-				ena		:	IN		STD_LOGIC;
-				rst		:	IN		STD_LOGIC;
-				up_J1   	:	IN		STD_LOGIC;
-				up_J2	   :	IN		STD_LOGIC;
-				down_J1	:	IN		STD_LOGIC;
-				down_J2	:	IN		STD_LOGIC;
-			   score1   :	OUT	STD_LOGIC_VECTOR(6 DOWNTO 0);
-			   score2   :	OUT	STD_LOGIC_VECTOR(6 DOWNTO 0);
-    Columnas_matriz1 :	OUT	STD_LOGIC_VECTOR(7  DOWNTO 0);
-	  Columnas_matriz2:	OUT	STD_LOGIC_VECTOR(7  DOWNTO 0);
-		Filas_matriz1	:	OUT	STD_LOGIC_VECTOR(7  DOWNTO 0);
-		Filas_matriz2	:	OUT	STD_LOGIC_VECTOR(7  DOWNTO 0));
-END ENTITY;
--------------------------------------------------------------------------------------
-ARCHITECTURE rtl OF PONG IS
-	TYPE Tablero IS ARRAY (15 DOWNTO 0) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
-	SIGNAL juego,ZEROS,game	:Tablero;
-	SIGNAL Rac1_Pos			:	STD_LOGIC_VECTOR(7 DOWNTO 0);
-	SIGNAL Rac2_Pos			:	STD_LOGIC_VECTOR(7 DOWNTO 0);
-	SIGNAL Position_Ballx   :	INTEGER;
-	SIGNAL Position_Bally   :	INTEGER;
-	SIGNAL Goal_1				:	STD_LOGIC;
-	SIGNAL Goal_2				:	STD_LOGIC;
-	SIGNAL marcador1_bin    :  STD_LOGIC_VECTOR(3 DOWNTO 0);
-	SIGNAL marcador2_bin    :  STD_LOGIC_VECTOR(3 DOWNTO 0);
-	SIGNAL clk_tablero		:	STD_LOGIC;
-	SIGNAL tick_speed       :  STD_LOGIC;
-
-	TYPE state IS (Initial, Desarrollo);
-	SIGNAL pr_state, nx_state: state;
--------------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+------------------------------------------------
+ENTITY bin_to_sseg IS
+	PORT ( bin	:	IN		STD_LOGIC_VECTOR(3 DOWNTO 0);
+			 sseg	:	OUT	STD_LOGIC_VECTOR(6 DOWNTO 0));
+END ENTITY bin_to_sseg;
+------------------------------------------------
+ARCHITECTURE behaviour OF bin_to_sseg IS
 BEGIN
-
-	
-	BALL:ENTITY work.Ball_Movement
-	PORT MAP(	clk 		=> clk,
-					ena 		=> ena,
-					rst 		=> rst,
-					Goal_1	=>	Goal_1,
-					Goal_2	=> Goal_2,
-					Rac1_Pos => Rac1_Pos,
-					Rac2_Pos => Rac2_Pos,
-			Position_Ballx => Position_Ballx,
-			Position_Bally => Position_Bally);
-		
-	
-	
-	timer_fps: ENTITY work.univ_bin_counter
-		GENERIC MAP(	N		=>	18	)
-		PORT MAP(	clk		=>	clk,
-						rst		=> rst,
-						ena		=>	ena,
-						syn_clr	=>	'0',
-						load		=>	'0',
-						up			=> '1',
-						d			=> "100100100111110000",
-						max_tick	=> clk_tablero	);
-
-	
-	
-	Matriz_1: ENTITY work.Visualization
-		PORT MAP(	clk		    =>	clk,
-						ena		    =>	ena,
-						rst		    =>	rst,
-				      columna_0    =>	juego(0),
-						columna_1    =>	juego(1),
-						columna_2    =>	juego(2),
-						columna_3    =>	juego(3),
-						columna_4    =>	juego(4),
-						columna_5    =>	juego(5),
-						columna_6    =>	juego(6),
-						columna_7    =>	juego(7),
-						Columna      =>	Columnas_matriz1,
-						Fila         =>	Filas_matriz1);
-	
-	
-	Matriz_2: ENTITY work.Visualization
-		PORT MAP(	clk		    =>	clk,
-						ena		    =>	ena,
-						rst		    =>   rst,
-						columna_0    =>	juego(8),
-						columna_1    =>	juego(9),
-						columna_2    =>	juego(10),
-						columna_3    =>	juego(11),
-						columna_4    =>	juego(12),
-						columna_5    =>	juego(13),
-						columna_6    =>	juego(14),
-						columna_7    =>	juego(15),
-						Columna      =>	Columnas_matriz2,
-						Fila         =>	Filas_matriz2);
-	
-	
-	
-	Racquet1: ENTITY work.Racquets_Movement1
-		PORT MAP (	clk		=>	clk,
-						rst		=>	rst,
-						ena      => ena,
-						up       => up_J1,
-						down     => down_J1,
-						Rac_Pos  => Rac1_Pos);
-						
-	
-	
-	Racquet2: ENTITY work.Racquets_Movement1
-		PORT MAP (	clk		=>	clk,
-						rst		=>	rst,
-						ena      => ena,
-						up       => up_J2,
-						down     => down_J2,
-						Rac_Pos  => Rac2_Pos);
-	
-	
-   Score:    ENTITY work.Score
-	PORT MAP (		clk		=>	clk,
-						rst		=>	rst,
-						Goal_1	=> Goal_1,
-						Goal_2   => Goal_2,
-					marcador1   => marcador1_bin,
-				   marcador2   => marcador2_bin);	
-						
-	
-	
-	bin_to_sseg_1: ENTITY work.bin_to_sseg
-	PORT MAP (		bin  => marcador1_bin,
-						sseg => score1);
-						
-	
-	
-	bin_to_sseg_2: ENTITY work.bin_to_sseg
-	PORT MAP (		bin  => marcador2_bin,
-						sseg => score2);				
-						
-		
-		timer_for_speed: ENTITY work.univ_bin_counter
-	GENERIC MAP(	N 			=> 28)
-	PORT MAP (		clk		=>	clk,
-						rst		=>	rst,
-						ena		=>	ena,
-						syn_clr	=>	'0',
-						load		=>	'0',
-						d			=> "0101111101011110000100000000",
-						max_tick => tick_speed);
-
-ZEROS	<=( 
-"11111111",
-"11111111",
-"11111111",
-"11111111",
-"11111111",
-"11111111",
-"11111111",
-"11111111",
-"11111111",
-"11111111",
-"11111111",
-"11111111",
-"11111111",
-"11111111",
-"11111111",
-"11111111");		
-		
-		
-		PROCESS(rst,ena,clk,clk_tablero,ZEROS)
-			BEGIN
-				IF(rst='1') THEN
-					juego <= ZEROS;
-				ELSIF(rising_edge(clk)) THEN
-					IF(clk_tablero = '1') THEN
-						pr_state <= nx_state;
-						juego <= game;
-					END IF;
-				END IF;
-			END PROCESS;
-
-	
-	PROCESS (pr_state,Rac1_Pos,Rac2_Pos,juego,Position_Ballx,Position_Bally,ZEROS)
-		BEGIN
-			CASE pr_state IS
-				WHEN Initial =>
-					game <= ZEROS;
-					nx_state<= Desarrollo;
-				WHEN Desarrollo =>
-					game <=(OTHERS => "11111111");
-					game(0) <=  Rac1_Pos;
-					game(15)<=  Rac2_Pos;
-					game(Position_Ballx)(Position_Bally) <= '1';
-					nx_state <= Initial;
-			END CASE;
-		END PROCESS;		
-
-
-END ARCHITECTURE;
+	WITH bin SELECT
+		sseg <= "1000000" WHEN "0000",
+				  "1111001" WHEN "0001",
+				  "0100100" WHEN "0010",
+				  "0110000" WHEN "0011",
+				  "0011001" WHEN "0100",
+				  "0010010" WHEN "0101",
+				  "0000010" WHEN "0110",
+				  "1111000" WHEN "0111",
+				  "0000000" WHEN "1000",
+				  "0010000" WHEN "1001",
+				  "0001000" WHEN "1010", --A
+				  "0000011" WHEN "1011", --B
+				  "1000110" WHEN "1100", --C
+				  "0100001" WHEN "1101", --D
+				  "0000110" WHEN "1110", --E
+				  "0001110" WHEN OTHERS; --F
+				  
+END behaviour;
+				  
